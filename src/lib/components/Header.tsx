8f672d20edcd8479511119853847b9d92c1afcbc
Sarthak Gupta <sarthak@vadelabs.com>
2022-03-13 21:00:00 +0530

    add anims

--- src/lib/components/Header.tsx
+++ src/lib/components/Header.tsx
@@ -1,10 +1,53 @@
 import React from "react";
+import HireMe from "./HireMe";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { motion } from "framer-motion";
+import { useState } from "react";
 
 const Header = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
-    <header className="w-full flex h-[10vh] items-center px-6 bg-navy drop-shadow-xl sticky top-0 text-white justify-between rounded-b-lg">
-      <Logo />
-    </header>
+    <>
+      <header className="w-full flex h-[10vh] items-center px-6 bg-navy drop-shadow-xl sticky top-0 text-white justify-between rounded-b-lg z-20">
+        <Logo />
+        <div className="flex items-center space-x-4 fill-current text-red">
+          <HireMe />
+          <GiHamburgerMenu
+            className="w-6 h-6"
+            onClick={() => setIsOpen(!isOpen)}
+          />
+        </div>
+      </header>
+      <>
+        {isOpen && (
+          <motion.div
+            className="h-screen w-screen bg-black opacity-50 fixed top-0 left-0 blur-md"
+            initial={{
+              opacity: 0,
+            }}
+            onClick={() => setIsOpen(false)}
+            animate={{ opacity: 1 }}
+          ></motion.div>
+        )}
+        <motion.div
+          className="h-screen w-3/4 fixed right-0 top-0 bg-gray-700 z-10"
+          animate={isOpen ? "visible" : "invisible"}
+          transition={{
+            ease: "easeOut",
+            bounce: 0.25,
+          }}
+          variants={{
+            visible: {
+              x: 0,
+            },
+            invisible: {
+              x: 300,
+            },
+          }}
+        ></motion.div>
+      </>
+    </>
   );
 };
 
---
+++ src/lib/components/HireMe.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const HireMe = () => {
+  return <div className='bg-blakc rounded-md drop-shadow-xl font-semibold text-white p-2'>Hire Me</div>;
+};
+
+export default HireMe;
