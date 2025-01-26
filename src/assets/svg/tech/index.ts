4b424c5c4f931bd42c091e31d8b81aa8dc6f545a
Sarthak Gupta <sarthak.ag2001@gmail.com>
2025-01-26 17:37:50 +0530

    feat: employment history

---
+++ src/assets/svg/tech/index.ts
@@ -0,0 +1,37 @@
+import ClojureSVG from "./clojure.svg";
+import DatomicSVG from "./datomic.svg";
+import DenoJSSVG from "./deno.svg";
+import FigmaSVG from "./figma.svg";
+import FirebaseSVG from "./firebase.svg";
+import FlaskSVG from "./flask.svg";
+import GoogleCloudSVG from "./gcp.svg";
+import NextJSSVG from "./next.svg";
+import NginxSVG from "./nginx.svg";
+import NodeJSSVG from "./node.svg";
+import PGSQLSVG from "./pgsql.svg";
+import PythonSVG from "./python.svg";
+import ReactSVG from "./react.svg";
+import ReduxSVG from "./redux.svg";
+import TailwindCSSSVG from "./twind.svg";
+import TypeScriptSVG from "./ts.svg";
+
+const Logos = {
+  ClojureSVG,
+  DatomicSVG,
+  DenoJSSVG,
+  FigmaSVG,
+  FirebaseSVG,
+  FlaskSVG,
+  GoogleCloudSVG,
+  NextJSSVG,
+  NginxSVG,
+  NodeJSSVG,
+  PGSQLSVG,
+  PythonSVG,
+  ReactSVG,
+  ReduxSVG,
+  TailwindCSSSVG,
+  TypeScriptSVG,
+};
+
+export default Logos;
